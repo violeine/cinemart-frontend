ff9983a77bd0775dc0fc243bdf9bac779dae02f5
violeine <haluudang@gmail.com>
2020-11-09 13:45:27 +0700

    change to inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Lato", ...defaultTheme.fontFamily.sans],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
