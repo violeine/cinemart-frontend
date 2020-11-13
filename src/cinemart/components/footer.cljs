766092b4ea63493b2d5f35617dbb15b9db452cb1
violeine <haluudang@gmail.com>
2020-11-14 01:08:09 +0700

    add credit to footer

--- src/cinemart/components/footer.cljs
+++ src/cinemart/components/footer.cljs
@@ -4,12 +4,26 @@
 (defn footer []
   [:footer.bg-gray-300.py-8.px-2
    [:div.xl:container.mx-auto
-    [:p.text-xl "Powered by"
-     (for [{:keys [alt src href]}
-           powered-by]
-       ^{:key alt}
-       [:a {:href href}
-        [:img.w-32.h-16.ml-4.inline {:src src
-                                     :alt alt}]])]]])
+    [:div.flex
+     [:p.text-xl {:class ["w-1/2"]}
+      "Powered by"
+      (for [{:keys [alt src href]}
+            powered-by]
+        ^{:key alt}
+        [:a {:href href}
+         [:img.w-32.h-16.ml-4.inline {:src src
+                                      :alt alt}]])]
+     [:p.text-xl.flex
+      {:class ["w-1/2"]}
+      [:span.mr-2 "Written by "]
+      [:a.mr-2 {:href "https://github.com/violeine/cinemart-frontend"
+           :target "_blank"}
+       [:img.w-16.rounded-full.h-16
+        {:src "https://github.com/violeine.png"
+         :alt "violeine"}]]
+      [:a.mr-2 {:href "https://github.com/anhvuk13/cinemart-backend"
+           :target "_blank"}
+       [:img.w-16.rounded-full.h-16
+        {:src "https://github.com/anhvuk13.png"}]]]]]])
 
 
