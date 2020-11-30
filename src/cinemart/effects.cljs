151d1d075bcc79f7776d751a39e37385ed43d55d
violeine <haluudang@gmail.com>
2020-11-30 21:14:54 +0700

    add localstorage and history effects

--- src/cinemart/effects.cljs
+++ src/cinemart/effects.cljs
@@ -7,3 +7,30 @@
  ::navigate!
  (fn-traced [route]
             (apply rfe/push-state route)))
+
+(rf/reg-fx
+ ::back!
+ (fn-traced []
+            (.go js/window.history -1)))
+
+(rf/reg-fx
+ ::save-storage!
+ (fn-traced [[key val]]
+            (.setItem (.-localStorage js/window) key val)))
+
+(rf/reg-fx
+ ::get-storage!
+ (fn-traced [key]
+            (.getItem (.-localStorage js/window) key)))
+
+(rf/reg-fx
+ ::remove-key-storage!
+ (fn-traced [key]
+            (.removeItem (.-localStorage js/window) key)))
+
+(rf/reg-fx
+ ::clear-storage!
+ (fn-traced []
+            (.clear (.-localStorage js/window))))
+
+
